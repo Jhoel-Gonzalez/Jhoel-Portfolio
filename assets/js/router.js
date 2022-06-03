633ebf76c38bb76ccd85f1345c96f91f9c8d1b6b
Jhoel Gonzalez <jhoel834@gmail.com>
2022-06-03 19:33:15 -0400

    adicionando nova rota no arquivo router.js

--- assets/js/router.js
+++ assets/js/router.js
@@ -2,4 +2,5 @@ $(function() {
     $("#header").load("header.html");
     $("#footer").load("footer.html");
     $("#tecnologias").load("tecnologias.html");
+    $("#empresas").load("empresas.html");
 });

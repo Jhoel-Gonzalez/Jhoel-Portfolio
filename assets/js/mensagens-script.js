94e4f46bf55a93f05116dc80d9ac3bcd31a020f7
Jhoel Gonzalez <jhoel834@gmail.com>
2022-06-05 12:46:26 -0400

    adicionando ponto e virgula no final das linhas 1,2,3,4,10, e 17, e removendo a const body.

--- assets/js/mensagens-script.js
+++ assets/js/mensagens-script.js
@@ -1,20 +1,19 @@
-const mensagens = localStorage.getItem("mensagem")
-const objeto = JSON.parse(mensagens)
-const listaMensagens = document.querySelector("#lista-mensagens")
-const body = document.querySelector("body")
-const botaoLimpar = document.querySelector("#botao-limpar")
+const mensagens = localStorage.getItem("mensagem");
+const objeto = JSON.parse(mensagens);
+const listaMensagens = document.querySelector("#lista-mensagens");
+const botaoLimpar = document.querySelector("#botao-limpar");
 
 let contador = 0;
 
 for (let mensagem of objeto) {
-    contador++
+    contador++;
     let pessoa = document.createElement("tr");
     pessoa.innerHTML = `<th scope="row">${contador}</th>
                         <td>${mensagem.nome}</td>
                         <td>${mensagem.email}</td>
                         <td>${mensagem.mensagem}</td>`;
     listaMensagens.appendChild(pessoa);
-}
+};
 
 botaoLimpar.addEventListener("click", (event) => {
     event.preventDefault();
